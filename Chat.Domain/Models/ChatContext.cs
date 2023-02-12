d10626fe7718e05a59fbf4f5129304fdad4fcf86
Ali Küçükkaya <108999321+ExuviaeSubarashii@users.noreply.github.com>
2023-02-12 17:43:52 +0300

    Update ChatContext.cs

--- Chat.Domain/Models/ChatContext.cs
+++ Chat.Domain/Models/ChatContext.cs
@@ -25,7 +25,7 @@ namespace Chat.Domain.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-12NGJ7T;Initial Catalog=Chat;Integrated Security=True");
+                optionsBuilder.UseSqlServer("ConnectionStringHere");
             }
         }
 
